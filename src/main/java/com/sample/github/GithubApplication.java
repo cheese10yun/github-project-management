afdb7eb6d015a7b45bcde617fa551b55ba56c074
Yun <cheese10yun@gmail.com>
2018-07-08 02:22:42 +0900

    first commit
    Signed-off-by: Yun <cheese10yun@gmail.com>

---
+++ src/main/java/com/sample/github/GithubApplication.java
@@ -0,0 +1,12 @@
+package com.sample.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubApplication.class, args);
+    }
+}
