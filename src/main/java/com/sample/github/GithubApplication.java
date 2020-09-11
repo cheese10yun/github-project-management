ca5f5cef1e7fe42a9f8a908bb2f56074cbc6d2c4
cheese10yun <cheese10yun@gmail.com>
2020-09-11 23:15:07 +0900

    Modify test template

--- src/main/java/com/sample/github/GithubApplication.java
+++ src/main/java/com/sample/github/GithubApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GithubApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GithubApplication.class, args);
     }
